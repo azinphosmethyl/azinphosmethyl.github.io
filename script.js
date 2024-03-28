cbc6b49a7efe13aae423dc72c899e162f8062e66
azinphosmethyl <midmax60@gmail.com>
2024-03-28 01:08:16 +0100

    g59 love

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("body").classList.add("loaded");
     }, 10)
   });
+  function aktualizujDate() {
+    document.getElementById('data').innerText = new Date().toLocaleString();
+  }
+  setInterval(aktualizujDate, 1000);
+  document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        document.title = ":(((";
+    } else {
+        document.title = ":)))";
+    }
+});
