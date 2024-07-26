3a85db2edfbb9b954098a965dc5117f32dc0d0fc
azinphosmethyl <midmax60@gmail.com>
2024-07-26 23:08:00 +0200

    i forgor i made this :skull:
    like fr

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.querySelector("body").classList.add("loaded");
-    }, 10)
-  });
-  function aktualizujDate() {
+window.onload = function() {
+    var navItems = document.querySelectorAll('nav ul li a');
+
+    navItems.forEach(function(item) {
+        item.addEventListener('mouseover', function() {
+            navItems.forEach(function(i) {
+                if (i !== item) {
+                    i.style.color = 'rgba(170, 170, 175, .88)';
+                }
+            });
+        });
+
+        item.addEventListener('mouseout', function() {
+            navItems.forEach(function(i) {
+                if (i !== item) {
+                    i.style.backgroundColor = '';
+                    i.style.color = '';
+                }
+            });
+        });
+    });
+};
+function aktualizujDate() {
     document.getElementById('data').innerText = new Date().toLocaleString();
   }
   setInterval(aktualizujDate, 1000);
-  document.addEventListener('visibilitychange', function() {
-    if (document.hidden) {
-        document.title = ":(((";
-    } else {
-        document.title = ":)))";
-    }
-});
